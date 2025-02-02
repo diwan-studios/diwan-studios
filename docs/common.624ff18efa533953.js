d598ccef3675f5dbb9faa77caa7128b992429744
Youssef Mansour <yousef.mansour@inovola.com>
2025-02-02 15:49:20 +0200

    deploy

--- docs/common.624ff18efa533953.js
+++
@@ -1,1 +0,0 @@
-"use strict";(self.webpackChunkamr_diwan_web=self.webpackChunkamr_diwan_web||[]).push([[592],{7130:(i,t,s)=>{s.d(t,{s:()=>a});let a=(()=>{class e{static#t=this.Default="home";static#s=this.Home="home";static#a=this.ArtWorks="artworks";static#e=this.Deft="deft";static#i=this.About="about";static#c=this.Careers="careers";static#_=this.Contact="contactus"}return e})()}}]);
